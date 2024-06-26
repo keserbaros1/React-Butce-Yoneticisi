8a070c5abde4eb3bf199d90791a263d83390c806
Barış <100820351+keserbaros1@users.noreply.github.com>
2024-06-26 23:40:56 +0300

    Ornek liste oluşturuldu. Girilen veriyi işlemek için liste açıldı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
+
 import Yenikayit from "./components/Yenikayit";
 import Giris from "./components/Giris";
+import HesapHareketleri from './components/HesapHareketleri';
 
 
 function App() {
+
+
+  const testListe = [
+    {
+      id: "e1",
+      title: "New Phone",
+      amount: 99.67,
+      date: new Date(2024, 1, 1),
+      id: Math.random().toString(),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2024, 1, 1),
+      id: Math.random().toString(),
+    },
+    {
+      id: "e3",
+      title: "Toilet Paper",
+      amount: 80.0,
+      date: new Date(2024, 5, 6),
+      id: Math.random().toString(),
+    },
+    {
+      id: "e4",
+      title: "Desk (Wooden)",
+      amount: 120.67,
+      date: new Date(2024, 3, 3),
+      id: Math.random().toString(),
+    },
+    {
+      id: "e5",
+      title: "Udemy Course",
+      amount: 9.9,
+      date: new Date(2024, 4, 4),
+      id: Math.random().toString(),
+    },
+    {
+      id: "e6",
+      title: "Book",
+      amount: 29.9,
+      date: new Date(2024, 4, 4),
+      id: Math.random().toString(),
+    },
+  ];
+
+
+
+const [yeniListe, yeniListeAyarla] = useState(testListe);
+
+const Veri = (veri) => {
+  yeniListeAyarla((eskiListe) => {
+    const yeniVeri = [veri];
+    return [...yeniVeri, eskiListe];
+  });
+};
+console.log(yeniListe)
+
 
   return (
     <div>
@@ -14,8 +75,14 @@ function App() {
 
 
       <div>
-        <Yenikayit IslenenVeriKayit></Yenikayit>
+        <Yenikayit IslenenVeriKayit={Veri}/>
       </div> 
+
+      <div>
+        <HesapHareketleri items={yeniListe}></HesapHareketleri>
+      </div>
+
+
     </div>
   );
   
