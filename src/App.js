5722aa881e625efcaf1d86dc6d5a1ec5994f0117
Barış <100820351+keserbaros1@users.noreply.github.com>
2024-06-26 05:39:10 +0300

    Giris yapma butonunun ön kısmı tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import Yenikayit from "./components/Yenikayit";
+import Giris from "./components/Giris";
 
 
 function App() {
 
   return (
+    <div>
       <div>
-      <Yenikayit IslenenVeriKayit></Yenikayit>
+        <Giris/>
       </div>
+
+
+      <div>
+        <Yenikayit IslenenVeriKayit></Yenikayit>
+      </div> 
+    </div>
   );
   
 }
