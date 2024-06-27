b070f54845053fa38fa9b274b3f75d953fac1c09
Barış <100820351+keserbaros1@users.noreply.github.com>
2024-06-27 21:52:39 +0300

    Kayıtların eklenmesi yapılıyor

---
+++ src/components/HesapKayitlari/KayitListesi.js
@@ -0,0 +1,106 @@
+import React, {useState} from "react";
+import {Table, Tag, Card, Input } from "antd";
+import RaporGoner from "./RaporGoner"; 
+import moment from "moment";
+
+
+
+
+
+const KayitListesi = (props) => {
+  
+  
+  
+  const [arananVeri, ArananVeriAyarla ] = useState('');
+  
+  let dataSource = [];
+  let columns = [
+    {
+      title: 'Harcamalar',
+      dataIndex: 'isim',
+      key: 'isim',
+      filteredValue: [arananVeri],
+      onFilter: (value, record) => {
+        return String(record.isim)
+        .toLowerCase()
+        .includes(value.toLowerCase()) ||
+
+        String(record.age)
+        .toLowerCase()
+        .includes(value.toLowerCase()) ||
+
+        String(record.tags)
+        .toLowerCase()
+        .includes(value.toLowerCase()) ||
+
+        String(record.date)
+        .toLowerCase()
+        .includes(value.toLowerCase());
+
+      },
+    },
+    {
+      title: 'Miktar',
+      dataIndex: 'age',
+      key: 'age',
+      sorter: (a, b) => a.age - b.age,
+    },
+    {
+      title: 'Tür',
+      dataIndex: 'tags',
+      key: 'tags',
+      render: tags => (
+        <>
+          {tags.map(tag => (
+            <Tag color="blue" key={tag}>
+              {tag}
+            </Tag>
+          ))}
+        </>
+      ),
+    },
+    {
+      title: 'Tarih',
+      dataIndex: 'date',
+      key: 'date',
+      render: date => moment(date).format('YYYY-MM-DD'),
+    },
+  ];
+
+  if (props.items.length > 0) {
+    dataSource = props.items.map(item => ({
+      key: item.id,
+      isim: item.title,
+      age: item.amount,
+      tags: [item.tur],
+      date: item.date,
+    }));
+  }
+
+
+
+  <RaporGoner 
+  kayitlar={dataSource} 
+  />
+      
+  
+      return(
+      <Card>
+        <RaporGoner/>
+        <Input.Search 
+        placeholder=""
+        onSearch={(value) => {ArananVeriAyarla(value);
+        }}
+        onChange={(e) => {
+          ArananVeriAyarla(e.target.value);
+        }}
+        />
+    <Table dataSource={dataSource} columns={columns} pagination={false} />
+    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+    </div> 
+   </Card>
+    );
+
+};
+
+export default KayitListesi;
