47edb72968eadc6e009148d541a998410f699bf9
Barış <100820351+keserbaros1@users.noreply.github.com>
2024-06-27 21:44:46 +0300

    syntax düzenlemesi

--- src/components/VeriHareketleri.js
+++ src/components/VeriHareketleri.js
@@ -3,44 +3,34 @@ import Card from "../UI/Card";
 
 
 import FiltreSecenekleri from "./FiltreSecenekleri";
-import KayitListesi from "./KayitListesi";
+import KayitListesi from "./HesapKayitlari/KayitListesi";
 import Istatistik from "./Istatistik";
 
 function VeriHareketleri(props) {
 
 const suanTarih = new Date().getFullYear();
-  const [secilenYil, secilenYilAyarla] = useState(suanTarih);
+  const [secilenYil, secilenYilAyarla] = useState(suanTarih.toString());
+
+
+
+
   const filtreDegistir = (yil) => {
-
-
-    console.log(props);
     secilenYilAyarla(yil);
   };
-
+  
   const filtrelenmisListe = props.items.filter((veri) => {
-    console.log(veri);
     return veri.date.getFullYear().toString() === secilenYil;
   });
 
   return (
     <Card className="expenses">
-      <FiltreSecenekleri
+      {/* <FiltreSecenekleri
         secim={secilenYil}
         filtre={filtreDegistir}
-      ></FiltreSecenekleri>
-      <Istatistik expenses={filtrelenmisListe} />
+      ></FiltreSecenekleri> */}
+      {/* <Istatistik expenses={filtrelenmisListe} /> */}
       <KayitListesi items={filtrelenmisListe} />
     </Card>
-
-    
-        // <Card className="expenses">     // ant design ile kart yap
-        //   <FiltreSecenekleri
-        //     secim ={selectedYear}
-        //     filtre ={filtreDegistir}
-        //   ></FiltreSecenekleri>
-        //   <Istatistik/>
-        //   <KayıtListesi/>
-        // </Card>
 
   );
 }
