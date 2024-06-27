8c8af4080792e1bf05959c1b13659191973415c1
Barış <100820351+keserbaros1@users.noreply.github.com>
2024-06-27 07:13:45 +0300

    zamana göre filtreleme yapıldı

---
+++ src/components/FiltreSecenekleri.js
@@ -0,0 +1,45 @@
+
+import { Form, DatePicker } from 'antd';
+import React, { useState } from 'react';
+import moment from 'moment';
+
+const FiltreSecenekleri = ({ secim, filtre }) => {
+
+
+
+  const girilenTarihDuzenle = (event) => { 
+    const tarih = moment(event);
+    const tarih2 = new Date(tarih);
+    filtre(tarih2.getFullYear());
+    console.log(tarih2)
+  };
+
+
+  const formattedDate = moment(secim);  // tarihi görüntüleyebilmek için formatlar
+
+
+
+    return (
+
+
+
+    <Form.Item label="Filter by year">
+    <DatePicker 
+    picker="year"
+    value={formattedDate}
+    onChange={girilenTarihDuzenle}
+    format={'YYYY'} 
+    allowClear={false}
+    />
+</Form.Item>
+
+
+
+    );
+  };
+
+
+  
+
+
+export default FiltreSecenekleri;
