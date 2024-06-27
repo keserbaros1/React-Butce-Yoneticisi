74d7c9b320a19267f241519b3dce41bca3cd8fe6
Barış <100820351+keserbaros1@users.noreply.github.com>
2024-06-27 03:13:09 +0300

    SEBEBİNİ ANLAMADIGIM VE ENERJİMİ SOMUREN HATA DUZELDİ SONUNDA ( hala sebebini bilmiyorum)

--- src/components/VeriHareketleri.js
+++ src/components/VeriHareketleri.js
@@ -1,34 +1,48 @@
-import React, { useState } from 'react';
-// import { List, Empty } from 'antd';
-import FiltreSecenekleri from './FiltreSecenekleri';
-// import Istasistik from './Istasistik';
-// import KayitListesi from "./KayıtListesi";
+import React, { useState } from "react";
+import Card from "../UI/Card";
 
 
+import FiltreSecenekleri from "./FiltreSecenekleri";
+import KayitListesi from "./KayitListesi";
+import Istatistik from "./Istatistik";
 
-function VeriHareketleri({ props }) {
+function VeriHareketleri(props) {
 
-    const suanTarih = new Date().getFullYear();
-    const [secilenYil, secilenYilAyarla] = useState(suanTarih);
+const suanTarih = new Date().getFullYear();
+  const [secilenYil, secilenYilAyarla] = useState(suanTarih);
+  const filtreDegistir = (yil) => {
 
-    const filtreDegistir = (yil) => {
-        secilenYilAyarla(yil);
-    }
 
-    const filtrelenmisListe = props.items.filter((veri) => {
-        return veri.date.getFullYear().toString() === secilenYil;
-    });
+    console.log(props);
+    secilenYilAyarla(yil);
+  };
 
-    return (
-        <Card className="expenses">     // ant design ile kart yap
-          <FiltreSecenekleri
-            secim ={selectedYear}
-            filtre ={filtreDegistir}
-          ></FiltreSecenekleri>
-          <Istasistik/>
-          <KayıtListesi/>
-        </Card>
-      );
-    }
+  const filtrelenmisListe = props.items.filter((veri) => {
+    console.log(veri);
+    return veri.date.getFullYear().toString() === secilenYil;
+  });
+
+  return (
+    <Card className="expenses">
+      <FiltreSecenekleri
+        secim={secilenYil}
+        filtre={filtreDegistir}
+      ></FiltreSecenekleri>
+      <Istatistik expenses={filtrelenmisListe} />
+      <KayitListesi items={filtrelenmisListe} />
+    </Card>
+
     
+        // <Card className="expenses">     // ant design ile kart yap
+        //   <FiltreSecenekleri
+        //     secim ={selectedYear}
+        //     filtre ={filtreDegistir}
+        //   ></FiltreSecenekleri>
+        //   <Istatistik/>
+        //   <KayıtListesi/>
+        // </Card>
+
+  );
+}
+
 export default VeriHareketleri;
